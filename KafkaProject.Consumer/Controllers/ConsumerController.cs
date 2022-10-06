afdd0c9a481037f99630b21418538171d059bdc9
elif.yilmaz <elif.yilmaz@ANTB-446>
2022-10-06 16:27:40 +0300

    Add project files.

---
+++ KafkaProject.Consumer/Controllers/ConsumerController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KafkaProject.Consumer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ConsumerController : ControllerBase
+    {
+    }
+}
